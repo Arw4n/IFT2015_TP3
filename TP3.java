985a35abccef4677b24bdfba9cabfce63f895ede
Arw4n <arnaudmehrabi@gmail.com>
2026-07-16 13:28:34 -0400

    Fichiers créés

---
+++ TP3.java
@@ -0,0 +1,18 @@
+public class TP3 {
+    /*
+    Input : 
+        fichier txt
+        par ligne : rue0 a b : rue, site1, site2
+        sites forment un graphe connexe étiqueté
+            Noeuds : sites
+            arêts : segments de rue
+            Poids : coût sur chaque segment
+    
+    But : créer un ARM des sites d'un quartier en utilisant Kruskal
+        Si deux arêtes de même poids : traitées dans l'ordre alphanumérique des noeuds de départ
+        Si l'ordre est aussi égal, ordre alphanumérique des noeuds d'arrivée
+    
+    
+    
+    */
+}
